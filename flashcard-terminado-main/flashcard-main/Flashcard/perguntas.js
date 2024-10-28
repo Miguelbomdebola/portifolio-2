17691e77622dff56152e1fa9054ae40b746bb7c6
Miguelbomdebola <137912910+Miguelbomdebola@users.noreply.github.com>
2024-10-28 08:48:29 -0300

    Add files via upload

---
+++ flashcard-terminado-main/flashcard-main/Flashcard/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Geografia',
+    'Que país tem o formato de uma bota?',
+    'Itália'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou Mona Lisa?',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'Futebol',
+    'Em que ano o Brasil ganhou a sua ultima copa do mundo?',
+    'Foi em 2002'
+)
+
+criaCartao(
+    'História',
+    'Quando terminou a Segunda Guerra Mundial?',
+    'Em 1945'
+)
+
+criaCartao(
+    'Filosofia',
+    ' De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes'
+)
+
+criaCartao(
+    'História',
+    'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'João Goulart'
+)
